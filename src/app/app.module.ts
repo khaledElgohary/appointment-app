5c52840ae420ad813e8b44d189db785aadd7d67e
khaledElgohary <121522162+khaledElgohary@users.noreply.github.com>
2024-09-20 03:48:20 -0500

    ..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {FormsModule} from '@angular/forms'
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
@@ -12,6 +12,7 @@ import { AppointmentListComponent } from './appointment-list/appointment-list.co
     AppointmentListComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     BrowserAnimationsModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
